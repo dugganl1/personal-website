e3560b54c07985022b8e9fdbda9e7e5588bc8488
Liam Duggan <dugganl1@tcd.ie>
2024-08-26 19:10:34 +0100

    just messing around now, miles off

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
-// Wait for the DOM to be fully loaded
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("Page loaded!");
-  // Your JavaScript code goes here
+// DOM manipulation module
+const DOM = (() => {
+  const getElement = (selector) => document.querySelector(selector);
+  const getAllElements = (selector) => document.querySelectorAll(selector);
+
+  return {
+    getElement,
+    getAllElements,
+  };
+})();
+
+// Blog module
+const Blog = (() => {
+  const init = () => {
+    console.log("Blog initialized");
+    // Add any initialization logic here
+  };
+
+  return {
+    init,
+  };
+})();
+
+// Event listeners
+document.addEventListener("DOMContentLoaded", () => {
+  Blog.init();
 });
